62801cfa5b3737ac3763a664614948310ddfcd80
look1z <381769096@qq.com>
2018-11-09 16:48:19 +0800

    补充了密码重置

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -4,13 +4,35 @@ from django.conf import settings
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #url(r'^login/$', views.user_login, name="user_login"),
     url(r'^login/$', auth_views.login, name="user_login"),
     url(r'^blog/', auth_views.logout, name="user_logout"),
-    url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"}, name="user_logout"),
+    url(r'^logout/$', auth_views.logout, {
+        "template_name": "account/logout.html"}, name="user_logout"),
     url(r'^register/$', views.register, name="user_register"),
-    url(r'^password-change/$', auth_views.password_change, name="password_change"),
-    url(r'^password-change-done/$', auth_views.password_change_done, name="password_change_done"),
+    url(r'^password-change/$', auth_views.password_change, {
+        "post_change_redirect": "/account/password-change-done"},
+        name="password_change"),
+    url(r'^password-change-done/$', auth_views.password_change_done,
+        name="password_change_done"),
+    url(r'^password-reset/$', auth_views.password_reset, {
+        "template_name": "account/password_reset_form.html",
+        "email_template_name": "/account/password_reset_email.html",
+        "subject_template_name": "/account/password_reset_subject.txt",
+        "post_reset_redirect": "/account/password_reset_done.html",
+    },
+        name="password_reset"),
+    url(r'^password-reset-done/$', auth_views.password_reset_done, {
+        "template_name": "account/password_reset_done.html"},
+        name="password_reset_done"),
+    url(r'^password-reset-confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        auth_views.password_reset_confirm,
+        {"template_name":"account/password_reset_confirm.html",
+         "post_reset_redirect":"/account/password-reset-complete"},
+        name="password_reset_confirm"),
+    url(r'^password-reset-complete/$', auth_views.password_reset_complete,
+        {"template_name": "account/password_reset_complete.html"},
+        name="password_reset_complete"),
+
 ]
 
 
